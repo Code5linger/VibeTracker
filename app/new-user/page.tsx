4b8854caab1df48b1d27ac46eada974f746cba3a
Code5linger <sakib.ahmed.1708@gmail.com>
2024-08-14 20:14:39 +0600

    !

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,3 +1,31 @@
-export const NewUser = () => {
-  return <div>i</div>;
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect('/journal');
 };
+
+const NewUser = async () => {
+  await createNewUser();
+  return <div>loading...</div>;
+};
+
+export default NewUser;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from '@clerk/nextjs/server';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  let href = userId ? '/journal' : '/new-user';
+
   return (
     <div className="w-screen h-screen bg-black text-white flex justify-center items-center">
       <div className="w-full max-w-[600px] mx-auto">
@@ -10,11 +15,9 @@ export default function Home() {
         <p className="text-2xl text-white/60 mb-4">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
           harum, quis assumenda ab, dolorem aperiam distinctio fugiat dolorum
-          asperiores architecto, voluptatibus quaerat. Culpa nam cupiditate
-          distinctio voluptatum enim hic voluptates!
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               Get Started
             </button>
